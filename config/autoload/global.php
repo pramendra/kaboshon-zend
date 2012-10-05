80530a75425bc05941a201fa46738cfe1d1a2463
Alexandr <ruderudebastrad@gmail.com>
2012-10-05 14:03:13 +0400

    1

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -3,7 +3,6 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=zend-training;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
